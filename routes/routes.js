8293e532bca856bf595b4141ebb07eabeaf4509b
bruindong <bruindong@gmail.com>
2018-05-15 15:48:38 -0700

    limit to 10 records returned

--- routes/routes.js
+++ routes/routes.js
@@ -26,7 +26,7 @@ var appRouter = function (app)
     app.get ("/headlines", function (req, res)
     {
         var headlines = [];
-        let sql = "SELECT h.hl_id, DATE_FORMAT(hl_datetime, '%l:%i %p') as hl_time, DATE_FORMAT(hl_datetime, '%m/%d/%y') as hl_date, h.hl_datetime, h.hl_headline, a.article_headline, a.article_url, a.article_source, a.article_description, a.article_favicon_url, a.article_thumbnail_url FROM newsDB.headlines AS h, newsDB.articles AS a WHERE h.hl_id = a.fk_hl_id ORDER BY h.hl_datetime DESC";
+        let sql = "SELECT h.hl_id, DATE_FORMAT(hl_datetime, '%l:%i %p') as hl_time, DATE_FORMAT(hl_datetime, '%m/%d/%y') as hl_date, h.hl_datetime, h.hl_headline, a.article_headline, a.article_url, a.article_source, a.article_description, a.article_favicon_url, a.article_thumbnail_url FROM newsDB.headlines AS h, newsDB.articles AS a WHERE h.hl_id = a.fk_hl_id ORDER BY h.hl_datetime DESC LIMIT 10";
 
         connection.query (sql, (error, results, fields) =>
         {
